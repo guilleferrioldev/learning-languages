0dfafaedc39f9c215e0cbd7f2ed243b82d49f0b2
guilleferrioldev <guillermoferrioldev@gmail.com>
2024-01-24 10:25:08 -0500

    created react template

---
+++ Typescript/template/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.Fragment>
+        <button>Hello World</button>
+        <button>Yeah</button>
+    </React.Fragment>
+);
