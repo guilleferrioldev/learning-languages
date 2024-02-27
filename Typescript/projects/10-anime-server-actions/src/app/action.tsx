63bee5fc7c4affc11ce03360134d2edaa26cb79c
guilleferrioldev <guillermoferrioldev@gmail.com>
2024-02-27 08:48:53 -0500

    finished 10-anime typescript project

--- Typescript/projects/10-anime-server-actions/src/app/action.tsx
+++ Typescript/projects/10-anime-server-actions/src/app/action.tsx
@@ -1,9 +1,15 @@
 "use server"
 
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
 export async function fetchAnime (page: number, limit: number) {
     const response = await fetch(
         `https://shikimori.one/api/animes?page=${page}&limit=${limit}&order=popularity`
     );
+    
     const data = await response.json();
-    return data;
+    
+    return data.map((item: AnimeProp, index: number) => (
+        <AnimeCard key={item.id} anime={item} index={index} />
+    ))
 }
--- Typescript/projects/10-anime-server-actions/src/components/LoadMore.tsx
+++ Typescript/projects/10-anime-server-actions/src/components/LoadMore.tsx
@@ -1,10 +1,36 @@
+"use client"
+
+import { useInView } from "react-intersection-observer";
 import Image from "next/image";
+import { useState , useEffect, useRef } from "react";
+import { fetchAnime } from "@/app/action";
+import AnimeCard from "./AnimeCard";
+
+export type AnimeCard = JSX.Element
 
 export default function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeCard[]>([])
+  const page = useRef(2)
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page.current, 8)
+        .then((res)=> {
+          setData([...data, ...res]);
+          page.current++;
+        })
+    }
+  }, [inView, data, page])
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
+
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
