8bb5ca9323a011c559823a33fee3c74b7c5fec30
guilleferrioldev <guillermoferrioldev@gmail.com>
2024-03-05 08:37:16 -0500

    created multisteps project inn typescript

---
+++ Typescript/projects/12-multistep-form/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Form from "@/components/form";
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <Form />
+      </div>
+    </section>
+  );
+}
---
+++ Typescript/projects/12-multistep-form/tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '1.5rem',
+        lg: '2rem'
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/forms')]
+}
+export default config
